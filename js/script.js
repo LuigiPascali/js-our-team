0ac4d50321f7601c6294dc58ffc865302e6f1743
luigimariapascali <luigimariapascali@gmail.com>
2023-06-27 09:06:37 +0200

    aggiunti controlli al form

--- js/script.js
+++ js/script.js
@@ -66,17 +66,44 @@ const teamMembers = [
 //     teamContainer.innerHTML += memberInfo;
 //  });
 
+// /* 
+//     BONUS 2: Organizzare i singoli membri in card/schede.
+// */
+
+// // Seleziono il container delle card
+
+// const teamContainer = document.querySelector('#team-container');
+
+// // Itero attraverso ogni membro del team
+// teamMembers.forEach(member => {
+//     // Creo un elemento div per la card 
+//   const memberCard = `
+//     <div class="col-md-4">
+//       <div class="card">
+//         <img src="img/${member.foto}" class="card-img-top" alt="${member.nome}">
+//         <div class="card-body">
+//           <h3 class="card-title">${member.nome}</h3>
+//           <p class="card-text">${member.ruolo}</p>
+//         </div>
+//       </div>
+//     </div>
+//   `;
+
+// // Aggiungo l'elemento div della card al contenitore delle card del tema
+//   teamContainer.innerHTML += memberCard;
+// });
+
 /* 
-    BONUS 2: Organizzare i singoli membri in card/schede.
+  VERSIONE 2: possibilità di aggiungere una nuova card, tramite i dati
+  compilati nel form
 */
 
-// Seleziono il container delle card
-
+// Seleziono il contenitore delle card del team
 const teamContainer = document.querySelector('#team-container');
 
 // Itero attraverso ogni membro del team
 teamMembers.forEach(member => {
-    // Creo un elemento div per la card 
+  // Creo un elemento div per la card 
   const memberCard = `
     <div class="col-md-4">
       <div class="card">
@@ -89,9 +116,62 @@ teamMembers.forEach(member => {
     </div>
   `;
 
-// Aggiungo l'elemento div della card al contenitore delle card del tema
+  // Aggiungo l'elemento div della card al contenitore delle card del tema
   teamContainer.innerHTML += memberCard;
 });
 
+// Aggiungo un event listener al form per gestire l'aggiunta di un nuovo membro del team
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  // Impedisco il comportamento predefinito del form
+  event.preventDefault();
+
+  // Recupero i valori inseriti nel form
+  const nome = document.querySelector('#nome').value;
+  const ruolo = document.querySelector('#ruolo').value;
+  const foto = document.querySelector('#immagine').value;
+
+    // Controllo se uno dei campi è vuoto
+    if (!nome || !ruolo) {
+      alert('Per favore, compila tutti i campi del form.');
+      return;
+    }
+
+    // Controllo se nome o ruolo contengono numeri
+    if (!isNaN(nome) ||!isNaN(ruolo)) {
+      alert('Il nome e il ruolo non possono contenere numeri.');
+      return;
+    }
+
+  // Creo un nuovo oggetto con i valori inseriti nel form
+  const newMember = {
+    nome: nome,
+    ruolo: ruolo,
+    foto: foto
+  };
+
+  // Aggiungo il nuovo membro all'array teamMembers
+  teamMembers.push(newMember);
+
+  // Creo un nuovo elemento div per la card del nuovo membro
+  const memberCard = `
+    <div class="col-md-4">
+      <div class="card">
+        <img src="https://picsum.photos/200${newMember.foto}" class="card-img-top" alt="${newMember.nome}">
+        <div class="card-body">
+          <h3 class="card-title">${newMember.nome}</h3>
+          <p class="card-text">${newMember.ruolo}</p>
+        </div>
+      </div>
+    </div>
+  `;
+
+  // Aggiungo l'elemento div della card al contenitore delle card del tema
+  teamContainer.innerHTML += memberCard;
+
+  // Resetto il form
+  form.reset();
+});
 
 
